be5b710e0f5e8ada152ad351028a7f3331382cff
Akshay12kavungal <107609828+Akshay12kavungal@users.noreply.github.com>
2023-11-23 10:12:48 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
+
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
+}
+const sections = document.querySelectorAll('.section');
+
+sections.forEach(section => {
+  section.addEventListener('click', () => {
+    section.querySelector('.sub-sections').classList.toggle('active');
+    section.classList.toggle("open");
+  });
+});
+
+
